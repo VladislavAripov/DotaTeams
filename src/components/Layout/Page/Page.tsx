7d5e11b822ffad50b761133aeaf6f85b9f5b6100
Vladislav Aripov <v.aripov@hightech.group>
2022-11-12 03:09:18 +0300

    Поправил текст возможностей на главной странице. Сделал кнопки хедера активными, если пользователь находится на странице. Поправил цвета в antd таблице

--- src/components/Layout/Page/Page.tsx
+++ src/components/Layout/Page/Page.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Page.less';
+import { Link } from 'react-router-dom';
 import browserHistory from 'App/root/browserHistory';
 import { Pages } from 'constants/links';
 import TextButton from 'ui-kit/TextButton';
+import { redirect } from 'utils/browser';
 
 export interface IProps {
     solidBackground?: boolean;
 }
 
 export const Page: React.FC<React.PropsWithChildren<IProps>> = (props) => {
+    const isCurrentPage = (path: string) => window.location.pathname === path;
+
     return (
         <div className={`page ${props.solidBackground ? 'solid-background' : ''}`}>
             <div className={'header'}>
                 <TextButton
                     className={'logo'}
+                    active={isCurrentPage(Pages.Main.url)}
                     onClick={() => browserHistory.push(Pages.Main.url)}
                 >
                     DotaTeams
@@ -21,18 +26,21 @@ export const Page: React.FC<React.PropsWithChildren<IProps>> = (props) => {
                 <div className={'nav-bar'}>
                     <TextButton
                         className={'nav-item'}
+                        active={isCurrentPage(Pages.Commands.url)}
                         onClick={() => browserHistory.push(Pages.Commands.url)}
                     >
                         Команды
                     </TextButton>
                     <TextButton
                         className={'nav-item'}
+                        active={isCurrentPage(Pages.Players.url)}
                         onClick={() => browserHistory.push(Pages.Players.url)}
                     >
                         Игроки
                     </TextButton>
                     <TextButton
                         className={'nav-item'}
+                        active={isCurrentPage(Pages.Tournaments.url)}
                         onClick={() => browserHistory.push(Pages.Tournaments.url)}
                     >
                         Турниры
@@ -40,6 +48,7 @@ export const Page: React.FC<React.PropsWithChildren<IProps>> = (props) => {
                     <div className={'divider'} />
                     <TextButton
                         className={'nav-item'}
+                        active={isCurrentPage(Pages.Authorization.url)}
                         onClick={() => browserHistory.push(Pages.Authorization.url)}
                     >
                         ВОЙТИ
@@ -59,19 +68,19 @@ export const Page: React.FC<React.PropsWithChildren<IProps>> = (props) => {
                                 Команда разработки:
                             </div>
                             <div className={'member-info'}>
-                                Игорь Вячеславович - igorsimonov2548@gmail.com
+                                Игорь Вячеславович - <Link to={'#'} onClick={() => redirect('mailto:igorsimonov2548@gmail.com', true)}>igorsimonov2548@gmail.com</Link>
                             </div>
                             <div className={'member-info'}>
-                                Владислав Сергеевич - vlad.aripov.007@gmail.com
+                                Владислав Сергеевич - <Link to={'#'} onClick={() => redirect('mailto:vlad.aripov.007@gmail.com', true)}>vlad.aripov.007@gmail.com</Link>
                             </div>
                         </div>
                     </div>
                     <div className={'right-panel'}>
                         <div className={'funding-info'}>
-                            Поддержать проект
+                            <Link to={'#'} onClick={() => redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ', true)}>Поддержать проект</Link>
                         </div>
                         <div className={'feedback'}>
-                            Обратная связь: help@dota.teams
+                            Обратная связь: <Link to={'#'} onClick={() => redirect('mailto:help@dota.teams', true)}>help@dota.teams</Link>
                         </div>
                     </div>
                 </div>
