8499f388c0df66dabe8b65942580f20d1f0a5bac
Vladislav Aripov <v.aripov@hightech.group>
2022-12-09 20:48:03 +0300

    вынес списки команд и игроков в моки

---
+++ src/api/v1.0/mocks.ts
@@ -0,0 +1,44 @@
+import { ICommand } from "../types/v1.0/command";
+import { IPlayer } from "../types/v1.0/player";
+
+export const commandsList: ICommand[] = [
+    { id: 1, preview: '', title: 'minain command', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 2, preview: '', title: 'not minain command', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 3, preview: '', title: 'winners', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 4, preview: '', title: 'losers', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 5, preview: '', title: 'the best', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 6, preview: '', title: 'mans', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 7, preview: '', title: 'best europe', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 8, preview: '', title: 'fighters', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 9, preview: '', title: 'MAIN', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 10, preview: '', title: 'major winners', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 11, preview: '', title: 'русские', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 12, preview: '', title: 'sticky boys', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 13, preview: '', title: 'били боба', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 14, preview: '', title: 'завоеватели', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 15, preview: '', title: 'nice try', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 16, preview: '', title: 'bros', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 17, preview: '', title: 'englishmen', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 18, preview: '', title: 'топики', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 19, preview: '', title: 'one tap', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 20, preview: '', title: 'ez gamers', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 21, preview: '', title: 'caraul', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 22, preview: '', title: 'hipers', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+    { id: 23, preview: '', title: 'happy men', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as ICommand,
+];
+
+export const playersList: IPlayer[] = [
+    { id: 1, preview: '', name: 'minain', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 2, preview: '', name: 'california', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 3, preview: '', name: 'pirat', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 4, preview: '', name: 'teamka', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 5, preview: '', name: 'human', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 6, preview: '', name: 'man', region: 'AZ', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 7, preview: '', name: 'super man', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 8, preview: '', name: 'gigachad', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 9, preview: '', name: 'heroe', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 10, preview: '', name: 'microchel', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 11, preview: '', name: 'lover', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 12, preview: '', name: 'mirsa', region: 'EU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+    { id: 13, preview: '', name: 'важная персона', region: 'RU', winsRate: 94, winsMatchesCount: 12, loseMatchesCount: 1, totalMatchesCount: 13 } as IPlayer,
+];
